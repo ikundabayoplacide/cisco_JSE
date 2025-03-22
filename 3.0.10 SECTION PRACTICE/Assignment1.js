de5f2bb64e86701502ef6876e0f5036c6bad8591
IKUNDABAYO Placide <ikundabayoplacide500@gmail.com>
2025-03-22 17:03:03 +0200

    Add files via upload
    that is my updated file. by mistake, I was uploaded the wrong one. it will never happen again.

--- 3.0.10 SECTION PRACTICE/Assignment1.js
+++ 3.0.10 SECTION PRACTICE/Assignment1.js
@@ -1,5 +1,55 @@
-let name =window.prompt("please enter a name","Placide");
-name=name?name:"anonymous";
-let age=prompt("Hello",+name+"how old are you?");
+//3.0.10 SECTION PRACTICE
 
-alert(name+"is"+age+"yease old");
+// #1  Arithmetic operators
+/*
+Exercise 1: Fill in the missing operators to get the expected result (replace the underscore symbol with the appropriate operator):
+console.log(2 _ 3 _ 1);      // expected 7
+console.log(2 _ 4);       // expected 16
+console.log(5 _ 1);       // expected 5
+console.log(8 _ 2 _ 5 _ 2); // expected 39 
+*/
+
+// Answer #1:
+
+console.log(2*3+1); // the result will be 7
+console.log(2**4); // the result will be 16
+console.log(5*1); // the result will be 5
+console.log(8**2-5**2); // the result will be 39
+
+// #2 Comparision operators
+/*
+Exercise 2: Fill in the missing comparison operators in that such a way that all expressions result in true (replace the underscore symbol with the appropriate operator):
+
+
+console.log(4 * 5 _ 20);
+console.log(6 * 5 _ "30");
+console.log(-17 _ 0);
+console.log(25 _ 1);
+console.log(2 + 2 * 2 _ 4);
+*/
+
+// Answer #2
+console.log(4*5 == 20); // Answer will true
+console.log(6*5 =="30"); // Answer will true
+console.log(-17<0); // Answer will true
+console.log(25>1); // Answer will true
+console.log(2+2*2>4); // Answer will true
+
+// #3 Logical operators
+
+/*
+Exercise 3: Fill in the missing comparison operators in that such a way that all expressions result in true (replace the underscore symbol with the appropriate operator):
+
+
+console.log(true _ false);
+console.log(false _ false);
+console.log(false _ false _ true);
+console.log(true _ false _ false && true);
+*/
+
+// Answers 3
+
+console.log(true || false); // here we will get true
+console.log(false ||! false); // here we will get true
+console.log(false || false|| true); // here we will get true
+console.log(true || false || false && true); // here we will get true
